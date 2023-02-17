0cd1fdffa99859b36cd19751833e9826ed16c9cc
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-02-17 19:18:36 +0700

    add: about screen

---
+++ src/screens/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function About() {
+    return (
+        <View style={styles.container}>
+            <Text>fuck</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'black',
+        flex: 1
+    }
+});
