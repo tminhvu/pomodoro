ab4f9da90f5174357f2f636c29c9491e7a187170
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-02-20 21:55:14 +0700

    change: screens now do their own detection of whether theyre in landscape mode or not

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import circularProgressSlice from "./slices/circularProgressSlice";
-import screenSlice from "./slices/screenSlice";
 
 export const store = configureStore({
     reducer: {
-        screen: screenSlice,
         circularProgress: circularProgressSlice
     }
 })
