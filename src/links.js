9bbf4fa4f93bde7e62d1e60cfaecaae1af9b28cf
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-03-20 16:59:12 +0700

    fix: repo link

--- src/links.js
+++ src/links.js
@@ -1,5 +1,5 @@
 const portfolio = 'https://tminhvu.vercel.app/'
 const github = 'https://github.com/tminhvu'
-const repository = 'https://github.com/tminhvu'
+const repository = 'https://github.com/tminhvu/pomodoro'
 
 export { portfolio, github, repository }
